7eb89d38ae6f758c1edf39d759212cfd1fba79fb
Adam Wojnicki <wojnicki.adam1@gmail.com>
2020-01-08 19:41:28 +0100

    Improved welcome screen and basic script refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
-import { checkScript } from "./script";
-import { Hello } from "./newScript";
 import "../scss/main.scss";
-
-console.log("IT WORKS");
-document.querySelector(".js-scss").textContent = "js and scss works";
-document.body.appendChild(checkScript());
-
-const hello = new Hello();
-document.body.appendChild(hello.sayHello());
+console.log("IT WORKS!");
--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-export const checkScript = () => {
-  const newp = document.createElement("p");
-  newp.classList.add("js-scss");
-  newp.textContent = "Imported script works";
-  return newp;
-};
